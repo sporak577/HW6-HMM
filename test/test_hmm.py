2d1229092e26b4555e02a75c20710ecda94e1d93
sporak577 <sophie.porak@ucsf.edu>
2025-02-20 16:18:54 -0800

    added a test if the hmm runs on a dataset in test_mini_weather

--- test/test_hmm.py
+++ test/test_hmm.py
@@ -3,8 +3,6 @@ from hmm import HiddenMarkovModel
 import numpy as np
 
 
-
-
 def test_mini_weather():
     """
     TODO: 
@@ -22,10 +20,30 @@ def test_mini_weather():
     mini_hmm=np.load('./data/mini_weather_hmm.npz')
     mini_input=np.load('./data/mini_weather_sequences.npz')
 
+    #extract parameters from the .npz file 
+    hidden_states = mini_hmm['hidden_states']
+    observation_states = mini_hmm['observation_states']
+    prior_p = mini_hmm['prior_p']
+    transition_p = mini_hmm['transition_p']
+    emission_p = mini_hmm['emission_p']
 
+    #extract observation sequences and expected outputs
+    observations = mini_input['observation_state_sequence']
+    best_hidden_sequence = mini_input['best_hidden_state_sequence']
+    
+    #initialize the HMM model
+    hmm = HiddenMarkovModel(observation_states, hidden_states, prior_p, transition_p, emission_p)
 
+    forward_prob = hmm.forward(observations)
 
+    viterbi_path = hmm.viterbi(observations)
 
+    #converting numpy array to a list for proper comparison
+    best_hidden_sequence = best_hidden_sequence.tolist()
+    
+    #there is only a 'best_hidden_state_sequence in the mini_input file, not for forward hmm
+    assert viterbi_path == best_hidden_sequence
+    
 
     
    
